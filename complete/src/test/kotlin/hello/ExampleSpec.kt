b1f857db75e221e005357e0a394d345d84daaef4
Adam Ruka <adamruka85@gmail.com>
2016-02-21 21:37:47 +0100

    Added an example Spec written in Spek.

---
+++ complete/src/test/kotlin/hello/ExampleSpec.kt
@@ -0,0 +1,22 @@
+package hello
+
+import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.spek.api.Spek
+
+class ExampleSpec : Spek() { init {
+    given("an empty String Builder") {
+        val sb = StringBuilder()
+
+        on("adding a character to the builder") {
+            sb.append('c')
+
+            it("should not be empty") {
+                assertThat(sb).isNotEmpty()
+            }
+
+            it("should contain that character") {
+                assertThat(sb).contains("c")
+            }
+        }
+    }
+}}
