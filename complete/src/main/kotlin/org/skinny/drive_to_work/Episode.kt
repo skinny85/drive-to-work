ea37c960834a1675c7edfac1ba61f361bcae1937
Adam Ruka <adamruka85@gmail.com>
2016-03-19 12:35:28 +0100

    Rename the 'number' attribute to 'id' in Episode.

--- complete/src/main/kotlin/org/skinny/drive_to_work/Episode.kt
+++ complete/src/main/kotlin/org/skinny/drive_to_work/Episode.kt
@@ -1,6 +1,6 @@
 package org.skinny.drive_to_work;
 
-data class Episode (val number: Int, val title: String,
+data class Episode (val id: Int, val title: String,
                     val description: String, val url: String) {
     private constructor() : this(0, "", "", "")
 }
