58cf8c113e7c041514beed08d916ac876bd9ca1d
Adam Ruka <adamruka85@gmail.com>
2016-03-27 13:13:41 +0200

    Move the registration of the custom LocalDate serializer from the Entity field to the application config.

--- complete/src/main/kotlin/org/skinny/drive_to_work/Application.kt
+++ complete/src/main/kotlin/org/skinny/drive_to_work/Application.kt
@@ -2,10 +2,20 @@ package org.skinny.drive_to_work;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
+import java.time.LocalDate
 
 @SpringBootApplication
-open class Application
+open class Application {
+    @Bean
+    open fun jacksonBuilder(): Jackson2ObjectMapperBuilder {
+        val builder: Jackson2ObjectMapperBuilder = Jackson2ObjectMapperBuilder();
+        builder.serializerByType(LocalDate::class.java, LocalDateSerializer());
+        return builder;
+    }
+}
 
 fun main(args: Array<String>) {
-	SpringApplication.run(Application::class.java, *args);
+    SpringApplication.run(Application::class.java, *args);
 }
--- complete/src/main/kotlin/org/skinny/drive_to_work/Episode.kt
+++ complete/src/main/kotlin/org/skinny/drive_to_work/Episode.kt
@@ -1,10 +1,9 @@
 package org.skinny.drive_to_work;
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import java.time.LocalDate
 
 data class Episode(val id: Int, val title: String,
-                   @field:JsonSerialize(using = LocalDateSerializer::class) val releaseDate: LocalDate,
+                   val releaseDate: LocalDate,
                    val description: String, val url: String) {
     private constructor() : this(0, "", LocalDate.of(2016, 2, 29), "", "")
 }
